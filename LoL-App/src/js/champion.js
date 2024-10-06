d4cbed0c6744a18fe15781735a70b86d8e53a2fa
IsaiasTolP <toledop.isaias@gmail.com>
2024-10-06 23:59:21 +0100

    Estructura de prueba

--- LoL-App/src/js/champion.js
+++ LoL-App/src/js/champion.js
@@ -1,6 +1,10 @@
 export default class Champion{
     constructor(data){
-        
+        this.name = data.name;
+        this.img = data.image.full;
+        this.title = data.title;
+        this.blurb = data.blurb;
+        this.attack = data.info.attack;
     }
     
 }
--- LoL-App/src/js/index.js
+++ LoL-App/src/js/index.js
@@ -1,2 +1,24 @@
 import Champion from "./champion.js";
 
+const url = "https://ddragon.leagueoflegends.com/cdn/13.18.1/data/es_ES/champion.json";
+
+const button = document.querySelector("button");
+
+button.addEventListener("click", () => {
+    button.style.visibility = "hidden";
+});
+
+fetch(url).then(response => response.json())
+    .then(data => {
+        const champsData = data.data;
+        Object.keys(champsData).forEach(champKey => {
+            const champion = new Champion(champsData[champKey]);
+            // console.log(champion.attack);
+        });
+    });
+
+function displayChamps(){
+    document.getElementById("body").innerHTML +=
+    `<p>Escritura curiosa</p>`;
+}
+window.displayChamps = displayChamps; // Pongo la función en el espacio de nombres global para acceder desde HTML
