d9c18ada2c98da1570a26357b33d77267f3c15d2
IsaiasTolP <toledop.isaias@gmail.com>
2024-10-09 00:05:02 +0100

    Detalles de imagenes

--- LoL-App/src/js/detail.js
+++ LoL-App/src/js/detail.js
@@ -21,9 +21,29 @@ async function loadChampDetails() {
     catch (error){
         console.error("Error al cargar los datos", error);
     }
-    finally {
-        hideLoadingScreen();
+}
+
+async function getChampImages(champ) {
+    let imageIdx = 0;
+    let imagesUrls = [];
+    while (true) {
+        let imgUrl = `https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${champ.id}_${imageIdx}.jpg`;
+        try {
+            let response = await fetch(imgUrl);
+            if (response.ok) {
+                imagesUrls.push(imgUrl);
+                imageIdx++;
+            }
+            else {
+                break;
+            }
+        }
+        catch (error){
+            console.log("Error", error);
+            break;
+        }
     }
+    return imagesUrls
 }
 
 function hideLoadingScreen(){
@@ -31,11 +51,48 @@ function hideLoadingScreen(){
     document.getElementById("main").style.visibility = "visible";
 }
 
-function displayChampionDetails(champ) {
-    let imgUrl = `https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${champ.id}_0.jpg`
+let currSlide = 0;
+let slides = [];
+
+async function displayChampionDetails(champ) {
+    const imagesUrls = await getChampImages(champ);
+    
     document.getElementById("champImg").innerHTML +=`
-    <img src="${imgUrl}">
-    <span>${champ.name}</span>`;
+    <div id="carousel"><span id="champName">${champ.name}</span><br><span id="champTitle">${champ.title}</span></div>`;
+    for(let i = 0; i < imagesUrls.length; i++){
+        let img = document.createElement('img');
+        img.src = imagesUrls[i];
+        img.classList.add('carouselItem');
+        if (i === 0) {
+            img.classList.add('active');
+        }
+        document.getElementById("carousel").appendChild(img);
+        
+    }
+    slides = document.querySelectorAll(".carouselItem");
+    hideLoadingScreen();
+    showSlide(currSlide);
+    autoSlide();
+}
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.remove('active');
+        if (i === index) {
+            slide.classList.add('active');
+        }
+    });
+}
+
+function moveSlide(step) {
+    currSlide = (currSlide + step + slides.length) % slides.length;
+    showSlide(currSlide);
+}
+
+function autoSlide() {
+    setInterval(function() {
+        moveSlide(1);
+    }, 5000);
 }
 
 loadChampDetails();
